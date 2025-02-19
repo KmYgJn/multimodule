2f6dc86c4f77ff4472558192e68a167da9c80c93
김영진 <qazkyj0310@gmail.com>
2025-02-19 16:28:01 +0900

    Add common demo service because test componentscan when spring bean

--- api/src/main/java/dev/be/api/service/DemoService.java
+++ api/src/main/java/dev/be/api/service/DemoService.java
@@ -1,13 +1,18 @@
 package dev.be.api.service;
 
 import dev.be.common.enums.CodeEnum;
+import dev.be.common.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
+    private final CommonDemoService commonDemoService;
 
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
+        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
