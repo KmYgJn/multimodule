d2b20a8bf846f6dbe74e9ef075e195c967661eb7
김영진 <qazkyj0310@gmail.com>
2025-02-20 00:10:35 +0900

    Fix logic with db

--- api/src/main/java/dev/be/api/service/DemoService.java
+++ api/src/main/java/dev/be/api/service/DemoService.java
@@ -1,6 +1,8 @@
 package dev.be.api.service;
 
+import dev.be.common.domain.Member;
 import dev.be.common.enums.CodeEnum;
+import dev.be.common.repository.MemberRepository;
 import dev.be.common.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,14 +11,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DemoService {
     private final CommonDemoService commonDemoService;
+    private final MemberRepository  memberRepository;
 
     public String save() {
-        System.out.println(CodeEnum.SUCCESS.getCode());
-        System.out.println(commonDemoService.commonService());
+        memberRepository.save(Member.builder()
+                .name(Thread.currentThread().getName()).build());
         return "save";
     }
 
     public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println("DB size : " + size);
         return "find";
     }
 
