f1c4f5a6574a0c9a2b48d0171566820d7841ca6c
김영진 <qazkyj0310@gmail.com>
2025-02-19 17:24:17 +0900

    Add global exception handler

---
+++ api/src/main/java/dev/be/api/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package dev.be.api.exceptionhandler;
+
+import dev.be.api.exception.CustomException;
+import dev.be.api.response.CommonResponse;
+import dev.be.common.enums.CodeEnum;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public CommonResponse handlerCustomException(CustomException e) {
+        return CommonResponse.builder()
+                .returnCode(e.getReturnCode())
+                .returnMessage(e.getReturnMessage())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public CommonResponse handlerException(Exception e) {
+        return CommonResponse.builder()
+                .returnCode(CodeEnum.UNKNOWN_ERROR.getCode())
+                .returnMessage(CodeEnum.UNKNOWN_ERROR.getMessage())
+                .build();
+    }
+}
