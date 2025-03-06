b11bc00f22906d9d856548fd1f18025734e05de9
김영진 <qazkyj0310@gmail.com>
2025-03-07 01:47:38 +0900

    Fix mysql connection

--- api/src/main/java/dev/be/api/ApiApplication.java
+++ api/src/main/java/dev/be/api/ApiApplication.java
@@ -2,10 +2,14 @@ package dev.be.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication (
 		scanBasePackages = { "dev.be.api", "dev.be.common"}
 )
+@EntityScan("dev.be.common.domain")
+@EnableJpaRepositories(basePackages = "dev.be.common.repository")
 public class ApiApplication {
 
 	public static void main(String[] args) {
