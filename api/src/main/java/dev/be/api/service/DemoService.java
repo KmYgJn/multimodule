42d24c79b8b14b3c928bfdee346a903d1df51201
김영진 <qazkyj0310@gmail.com>
2025-03-11 00:42:58 +0900

    Check active profile

--- api/src/main/java/dev/be/api/service/DemoService.java
+++ api/src/main/java/dev/be/api/service/DemoService.java
@@ -5,15 +5,18 @@ import dev.be.common.enums.CodeEnum;
 import dev.be.common.repository.MemberRepository;
 import dev.be.common.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DemoService {
-    private final CommonDemoService commonDemoService;
+    @Value("${profile-name}")
+    private String name;
     private final MemberRepository  memberRepository;
 
     public String save() {
+        System.out.println("name : " + name);
         memberRepository.save(Member.builder()
                 .name(Thread.currentThread().getName()).build());
         return "save";
